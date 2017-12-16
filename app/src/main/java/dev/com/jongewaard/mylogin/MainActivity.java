8b1276d863ccc4629571d652ec52705607cafd0a
german <germanjongewaard@gmail.com>
2017-12-16 01:58:01 +0100

    Login commit

--- app/src/main/java/dev/com/jongewaard/mylogin/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/mylogin/MainActivity.java
@@ -2,6 +2,8 @@ package dev.com.jongewaard.mylogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -17,19 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //aquí pego el metodo con el codigo recogido
+        bindUI();
+
     }
 
-
+    /* Método para recojer el codigo*/
     private void bindUI() {
         /* Aquí los enlazo, recojo las referencias */
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
-        switchRemember = switchRemember;
-        btnLogin = btnLogin;
-
-
-
-
-
+        switchRemember = (Switch) findViewById(R.id.switchRemember);
+        btnLogin = (Button) findViewById(R.id.buttonLogin);
     }
+
+        private boolean isValidEmail(String email) {
+        //aqui nos dice si cumple con las reglas basicas de un correo electronico.
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        }
+
+
+
 }
