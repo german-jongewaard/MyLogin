d80ecb45a4a50cacb8e3a602340e8bec78c71f6c
german <germanjongewaard@gmail.com>
2017-12-16 01:47:05 +0100

    Login commit

--- app/src/main/java/dev/com/jongewaard/mylogin/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/mylogin/MainActivity.java
@@ -2,12 +2,34 @@ package dev.com.jongewaard.mylogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Switch switchRemember;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    private void bindUI() {
+        /* Aquí los enlazo, recojo las referencias */
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        switchRemember = switchRemember;
+        btnLogin = btnLogin;
+
+
+
+
+
     }
 }
