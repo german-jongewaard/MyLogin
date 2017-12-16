7244853111b7be21690555f505e7cb1e0a100806
german <germanjongewaard@gmail.com>
2017-12-16 02:14:09 +0100

    Login commit

--- app/src/main/java/dev/com/jongewaard/mylogin/LoginActivity.java
+++ app/src/main/java/dev/com/jongewaard/mylogin/LoginActivity.java
@@ -1,10 +1,11 @@
 package dev.com.jongewaard.mylogin;
 
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by german on 16-12-17.
  */
 
-public class LoginActivity   {
+public class LoginActivity extends AppCompatActivity {
 }
--- app/src/main/java/dev/com/jongewaard/mylogin/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/mylogin/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +26,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private boolean login(String email, String password){
+
+        if(!isValidEmail(email)){
+            //si el email falla, mensaje y falso.
+            Toast.makeText(this, "Email is not valid, please try again", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(!isValidPassword(password)){
+            //si el password falla, mensaje y falso.
+            Toast.makeText(this, "Password is not valid, 4 characters or more, please try again", Toast.LENGTH_LONG).show();
+            return false;
+        }else {
+            //si ninguno fallo es verdadero!
+            return true;
+        }
+
+    }
+
+
+
     /* Método para recojer el codigo*/
     private void bindUI() {
         /* Aquí los enlazo, recojo las referencias */
@@ -34,10 +54,15 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = (Button) findViewById(R.id.buttonLogin);
     }
 
-        private boolean isValidEmail(String email) {
+    private boolean isValidEmail(String email) {
         //aqui nos dice si cumple con las reglas basicas de un correo electronico.
         return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
-        }
+    }
+
+    private  boolean isValidPassword(String password) {
+        //que el password sea mayor de 4 caracteres!
+        return password.length() > 4;
+    }
 
 
 
