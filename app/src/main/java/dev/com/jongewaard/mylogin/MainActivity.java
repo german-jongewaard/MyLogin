1ae22c8ff8e724840321768a0e8bb456340a2b8b
german <germanjongewaard@gmail.com>
2017-12-17 02:24:39 +0100

    Login commit

--- app/src/main/java/dev/com/jongewaard/mylogin/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/mylogin/MainActivity.java
@@ -3,6 +3,7 @@ package dev.com.jongewaard.mylogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
