390fc733cf539d07a9a2df01649fdc4530d5c2b1
german <germanjongewaard@gmail.com>
2017-12-18 01:10:18 +0100

    Login commit

---
+++ app/src/main/java/dev/com/jongewaard/mylogin/SplashActivity.java
@@ -0,0 +1,20 @@
+package dev.com.jongewaard.mylogin;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by german on 18-12-17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+    }
+}
