f18935aed461d963d9ebf74aa5e4c8498d1e95b7
german <germanjongewaard@gmail.com>
2017-12-18 01:31:43 +0100

    Login commit

--- app/src/main/java/dev/com/jongewaard/mylogin/splash/SplashActivity.java
+++ app/src/main/java/dev/com/jongewaard/mylogin/splash/SplashActivity.java
@@ -1,8 +1,11 @@
 package dev.com.jongewaard.mylogin.splash;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 
 import dev.com.jongewaard.mylogin.activities.LoginActivity;
 
@@ -12,12 +15,22 @@ import dev.com.jongewaard.mylogin.activities.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private SharedPreferences prefs;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        //aqui ya tengo la instancia
+        prefs = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
 
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intentLogin = new Intent(this, LoginActivity.class);
+        Intent intentMain = new Intent(this, LoginActivity.class);
+
+
+        if(TextUtils.isEmpty())
+
+
         startActivity(intent);
     }
 }
